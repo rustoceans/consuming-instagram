e7a130dc2bea7ad6af3a956924709dfbdc527d20
alexfalcucci <alex.falcucci@hotmail.com>
2015-01-02 04:30:37 -0200

    get random users now, after to do and include news functions

--- consuming_instagram/core/views.py
+++ consuming_instagram/core/views.py
@@ -6,12 +6,11 @@ import json
 
 def home(request):
     access_token = '1525899796.5b9e1e6.48f9b8caae4d45059c9772b07fcadf55'
-    url = 'https://api.instagram.com/v1/tags/\
-           cachorros/media/recent?access_token=\
-           {access_token}'.format(access_token=access_token)
+    url = 'https://api.instagram.com/v1/tags/pets/media/recent?access_token={access_token}'.format(access_token=access_token)
     response = urllib2.urlopen(url)
     read = response.read()
     lenght_response = len(json.loads(read)['data'])
-    users = [json.loads(read)['data'][x]['user']['username'] for x in range(lenght_response)]
-
-    return render(request, 'core/base.html')
+    users = [json.loads(read)['data'][x]['user']['username']
+             for x in range(lenght_response)]
+    context = {'users': users, }
+    return render(request, 'core/base.html', context)
